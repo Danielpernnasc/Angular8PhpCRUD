e8b83bbcc510bc9c84953949a48b46be6cfb0b0b
Daniel Nascimento <dpericles6@gmail.com>
2020-06-16 16:16:30 -0300

    primeito commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewComponent } from './crud/view';
+import { CreateComponent } from './crud/create';
+import { UpdateComponent } from './crud/update';
+import { ReadComponent } from './crud/read';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path: '', component: ViewComponent},
+{path: 'create', component: CreateComponent},
+{path: 'update/:id', component: UpdateComponent},
+{path: 'view/:id', component: ReadComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ViewComponent } from './crud/view';
+import { CreateComponent } from './crud/create/create.component';
+import { UpdateComponent } from './crud/update/update.component';
+import { ReadComponent } from './crud/read/read.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    CreateComponent,
+    UpdateComponent,
+    ReadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
